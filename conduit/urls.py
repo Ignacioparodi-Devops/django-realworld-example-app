6b4f2f34db46a18c62eb7367d196fd5e86996eae
Ignacioparodi-Devops <60270235+Ignacioparodi-Devops@users.noreply.github.com>
2020-01-30 16:59:34 -0300

    Update urls.py

--- conduit/urls.py
+++ conduit/urls.py
@@ -17,9 +17,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 
-    path(r'^api/', include('conduit.apps.articles.urls', namespace='articles')),
-    path(r'^api/', include('conduit.apps.authentication.urls', namespace='authentication')),
-    path(r'^api/', include('conduit.apps.profiles.urls', namespace='profiles')),
+    url(r'^api/', include('conduit.apps.articles.urls', namespace='articles')),
+    url(r'^api/', include('conduit.apps.authentication.urls', namespace='authentication')),
+    url(r'^api/', include('conduit.apps.profiles.urls', namespace='profiles')),
 ]
